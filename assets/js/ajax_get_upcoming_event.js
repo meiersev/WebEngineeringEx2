908e9d8dbe95d35bab1003f4d57c9c6995056b3b
vilmarzti <vilmarti@student.ethz.ch>
2017-04-11 11:09:11 +0200

    history works, sort of

--- assets/js/ajax_get_upcoming_event.js
+++ assets/js/ajax_get_upcoming_event.js
@@ -1,9 +1,7 @@
 (function($) {
     jQuery('.container-element a').click( function(event){ 
-    /*$(document).on('click', '.container-element a', function( event ){*/
         event.preventDefault();
         var offset = $(this).attr("class");
-        console.log(offset);
         $.ajax({
             url: ajaxpagination.ajaxurl,
             type: 'post',
@@ -12,9 +10,25 @@
                 offset: offset,
             },
             success: function(result){
+                var stateobj = {upcoming: $("#upcoming_event_id").html() };
+                window.history.pushState(stateobj, "", "#upcoming");
+
                 $("#upcoming_event_id").find('.container-element').remove();
                 $("#upcoming_event_id").append(result);
+
+                stateobj = {upcoming: $("#upcoming_event_id").html() };
+                window.history.pushState(stateobj, "", "#upcoming");
+
+
             }
         });
     });
+
+    window.onpopstate = function(event){
+        var stateobj = {upcoming: $("#upcoming_event_id").html() };
+        window.history.pushState(stateobj, "", "#upcoming");
+
+        $("#upcoming_event_id").find('.container-element').remove();
+        $("#upcoming_event_id").append(event.state.upcoming);
+    };
 })(jQuery);
